ff0154722b2ddb9822a05afa85ab87863df5c05e
klyapov <mr.kyperr@mail.ru>
2017-03-18 23:49:31 +0300

    Update 1-Created_class.cpp

--- 1-Created_class.cpp
+++ 1-Created_class.cpp
@@ -1,4 +1,4 @@
-	// Ñîçäàíèå êëàññà - ëó÷íèê
+	// Создание класса - лучник
 	#ifndef CLASS_H_
 	#define CLASS_H_
 	class Monstr;
@@ -6,32 +6,32 @@
 		{
 			private:
 				int hitpoints, manapoints, damage, defence;
-				static const int NAMESIZE = 3; // îáúÿâëåíèå ñòàòè÷åñêîãî êîìïîíåíòà - êîíñòàíòíîãî çíà÷åíèÿ êîëè÷åñòâà èì¸í ëó÷íèêîâ
+				static const int NAMESIZE = 3; // объявление статического компонента - константного значения количества имён лучников
 			public:
-				static const string name[]; // îáúÿâëåíèå ñòàòè÷åñêîãî êîìïîíåíòà - èìåíè ëó÷íèêà
-				Archer (int hitpoints, int manapoints, int damage, int defence); // îáúÿâëåíèå êîíñòðóêòîðà õàðàêòåðèñòèê ëó÷íèêà ñ èñïîëüçîâàíèåì this
-				Archer (bool specialisation); // îáúÿâëåíèå êîíñòðóêòîðà ñïåöèàëèçàöèè ëó÷íèêà
-				Archer (string color_of_cloak, int number_of_cloak); // îáúÿâëåíèå êîíñòðóêòîðà öâåòà è íîìåðà ïëàùà ëó÷íèêà
-				Archer() // êîíñòðóêòîð ïåðâè÷íûõ õàðàòåðèñòèê ëó÷íèêà, ïî-óìîë÷àíèþ
+				static const string name[]; // объявление статического компонента - имени лучника
+				Archer (int hitpoints, int manapoints, int damage, int defence); // объявление конструктора характеристик лучника с использованием this
+				Archer (bool specialisation); // объявление конструктора специализации лучника
+				Archer (string color_of_cloak, int number_of_cloak); // объявление конструктора цвета и номера плаща лучника
+				Archer() // конструктор первичных харатеристик лучника, по-умолчанию
 					{
 						hitpoints = 0;
 						manapoints = 0;
 						damage = 0;
 						defence = 0;
 					}
-				~Archer() // îáúÿâëåíèå äåñòðóêòîðà ïî-óìîë÷àíèþ
+				~Archer() // объявление деструктора по-умолчанию
 					{
-						cout << "*\n"; // âûâîä íà ýêðàí ñèìâîëà *, åñëè áûë èñïîëüçîâàí äåñòðóêòîð
+						cout << "*\n"; // вывод на экран символа *, если был использован деструктор
 					}
-				void bow (const int ARROWS); // îáúÿâëåíèå ìåòîäà âûñòðåëîâ èç ëóêà
-				void buff (char orientation_buff, string name_buff, string effect_buff); // ìåòîä ïåðå÷èñëåíèÿ íàëîæåííûõ áàôîâ
-				void show (); // ìåòîä âûâîäà èíôîðìàöèè î õàðàêòåðèñòèêàõ ëó÷íèêà íà ýêðàí
-				friend void dead_archer (const Archer &name); // îáúÿâëåíèå äðóæåñòâåííîé ôóíêöèè
+				void bow (const int ARROWS); // объявление метода выстрелов из лука
+				void buff (char orientation_buff, string name_buff, string effect_buff); // метод перечисления наложенных бафов
+				void show (); // метод вывода информации о характеристиках лучника на экран
+				friend void dead_archer (const Archer &name); // объявление дружественной функции
 		};
-	void dead_archer (const Archer &name) // èíèöèàëèçàöèÿ äðóæåñòâåííîé ôóíêöèè
+	void dead_archer (const Archer &name) // инициализация дружественной функции
 		{
 			cout << "\n\t\t\t\t  The lifetime of the archer is over. Archer is dead. You characteristics:\n\t\t\t\tHP: " << name.hitpoints 
 			     << "\n\t\t\t\tMP: " << name.manapoints << "\n\t\t\t\tDMG: " << name.damage << "\n\t\t\t\tDEF: " << name.defence << endl;
 		}
-	const string Archer::name[NAMESIZE] = {"Shiro", "Robin", "Parys"}; // èíèöèàëèçàöèÿ ñòàòè÷åñêîãî êîìïîíåíòà - èì¸í ëó÷íèêîâ
+	const string Archer::name[NAMESIZE] = {"Shiro", "Robin", "Parys"}; // инициализация статического компонента - имён лучников
 	#endif
